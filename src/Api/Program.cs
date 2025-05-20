a048b12049d924f437438607b5db9856489ae1f4
Nicolas Liberatto <nicolas.liberatto@gmail.com>
2025-05-19 23:16:24 -0300

    DB First aproach initiated

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Api
 {
     public class Program
@@ -7,6 +9,9 @@ namespace Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            
+            //Register DbContext
+            
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/Infrastructure/Context/OracleContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class OracleContext : DbContext
+    {
+        public OracleContext(DbContextOptions<OracleContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Status> Status { get; set; }
+    }
+}
